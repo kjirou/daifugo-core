5951916c9ab2c77c8eebeb7d7b59634a4dbbceab
kjirou <sorenariblog@gmail.com>
2021-01-31 14:42:27 +0900

    Make to receive key inputs

--- examples/src/game.ts
+++ examples/src/game.ts
@@ -1,4 +1,25 @@
 import * as daifugo from '../../src/index'
 
+const keypress = require('keypress')
+
+type KepressKey = {
+  ctrl: boolean,
+  name: string,
+}
+
 let game = daifugo.utils.createGame()
-console.log(game)
+
+const handleKeypress = (ch: string, key: KepressKey): void => {
+  if (key.ctrl && key.name == 'c' || key.ctrl && key.name == 'd' || key.ctrl && key.name == 'q') {
+    process.exit(0)
+  }
+}
+
+// Settings for "keypress" module.
+keypress(process.stdin)
+process.stdin.on('keypress', handleKeypress)
+
+// Start the main process.
+process.stdout.write('Start the game.\n')
+process.stdin.setRawMode(true)
+process.stdin.resume()
